7b34feb791bcae2adc218b5bbef40d016e4ac452
AizeRs <NAKorshunov@mail.ru>
2021-05-28 19:04:37 +0500

    some changes

--- mod.ts
+++ mod.ts
@@ -8,6 +8,11 @@ const arr = [
     name: "Песня",
     songLink: "https://dnl.megapesni.com/get/online/2NaJvexHpOrn2YWZB9CrzQ/1622253415/1aa12212/2020/04/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%94%D0%B0%D0%B2%D0%B0%D0%B9%20%D0%9E%D1%81%D1%82%D0%B0%D0%BD%D0%B5%D0%BC%D1%81%D1%8F%20%D0%94%D0%BE%D0%BC%D0%B0.mp3",
     photoLink: "",
+  },
+  {
+    name: "Песня",
+    songLink: "https://dnl.megapesni.com/get/online/2NaJvexHpOrn2YWZB9CrzQ/1622253415/1aa12212/2020/04/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%94%D0%B0%D0%B2%D0%B0%D0%B9%20%D0%9E%D1%81%D1%82%D0%B0%D0%BD%D0%B5%D0%BC%D1%81%D1%8F%20%D0%94%D0%BE%D0%BC%D0%B0.mp3",
+    photoLink: "",
   }
 ]
 
