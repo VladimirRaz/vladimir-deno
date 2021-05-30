fd03dbe47608a95d7c38eab05cc92a3019a343d6
Vladimir <61233643+VladimirRaz@users.noreply.github.com>
2021-05-30 19:15:24 +0500

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -36,6 +36,265 @@ const arr = [
     songLink: "https://dnl.megapesni.com/get/online/80d2Fd-xja7uH5Fb33xpYA/1622343317/1aa12212/2020/04/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%94%D0%B0%D0%B2%D0%B0%D0%B9%20%D0%9E%D1%81%D1%82%D0%B0%D0%BD%D0%B5%D0%BC%D1%81%D1%8F%20%D0%94%D0%BE%D0%BC%D0%B0.mp3",
     photoLink: "",
     "check": false,
+  },
+    
+  {
+    name: "Лайкер",
+    songLink: "https://dnl.megapesni.com/get/online/kjOT041ZEr0ToCZ2kHKl9w/1622426265/f6dbadc2/2019/08/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9B%D0%B0%D0%B9%D0%BA%D0%B5%D1%80.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Улететь",
+    songLink: "https://dnl.megapesni.com/get/online/W8eqKfnVoaw-xTWMYvqQfA/1622426265/7b7cd4f5/2017/04/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%A3%D0%BB%D0%B5%D1%82%D0%B5%D1%82%D1%8C.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Розовые Очки",
+    songLink: "https://dnl.megapesni.com/get/online/QT0dXGOzjliSBgXYZ6oKvg/1622426265/68668f22/2021/01/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%A0%D0%BE%D0%B7%D0%BE%D0%B2%D1%8B%D0%B5%20%D0%9E%D1%87%D0%BA%D0%B8.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Грустный Трек",
+    songLink: "https://dnl.megapesni.com/get/online/uAFiUYIbKZ6Mm12C54MNqQ/1622426265/5cfff461/2021/02/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%93%D1%80%D1%83%D1%81%D1%82%D0%BD%D1%8B%D0%B8%CC%86%20%D0%A2%D1%80%D0%B5%D0%BA.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "WIFI",
+    songLink: "https://dnl.megapesni.com/get/online/4NYxCgxu6w3TIplB7yw_aQ/1622426265/7c1b5500/2017/12/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20WIFI.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Водица",
+    songLink: "https://dnl.megapesni.com/get/online/5enrlQAvNTGF2T5wVnPomw/1622426265/927c0689/2019/06/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%92%D0%BE%D0%B4%D0%B8%D1%86%D0%B0.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Ночь Текила",
+    songLink: "https://dnl.megapesni.com/get/online/_oJltzdMg_rmJS7jUoU9Ww/1622426265/acfa4e58/2018/07/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9D%D0%BE%D1%87%D1%8C%20%D0%A2%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "По Сердцу",
+    songLink: "https://dnl.megapesni.com/get/online/Q3vWKx_xO3S8LIU1zODZ4Q/1622426265/e3eac876/2018/10/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9F%D0%BE%20%D0%A1%D0%B5%D1%80%D0%B4%D1%86%D1%83.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Вот Она Я",
+    songLink: "https://dnl.megapesni.com/get/online/KqoX3YsoJUYNf0OJuj42nQ/1622426588/34aa656c/2019/09/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%92%D0%BE%D1%82%20%D0%9E%D0%BD%D0%B0%20%D0%AF.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Одна Ночь",
+    songLink: "https://dnl.megapesni.com/get/online/bUzfsTMYTgljpE1WDcT7LA/1622426588/7c2356ce/2018/01/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9E%D0%B4%D0%BD%D0%B0%20%D0%9D%D0%BE%D1%87%D1%8C.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Равновесие",
+    songLink: "https://dnl.megapesni.com/get/online/cUAJ5EvOCEqMUZXiFoiY8A/1622426588/a5d469da/2017/10/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%A0%D0%B0%D0%B2%D0%BD%D0%BE%D0%B2%D0%B5%D1%81%D0%B8%D0%B5.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "В Огне",
+    songLink: "https://dnl.megapesni.com/get/online/U5jeM-ehr-phui2PECqHfg/1622426588/18035ba6/2020/01/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%92%20%D0%9E%D0%B3%D0%BD%D0%B5.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Неправильная",
+    songLink: "https://dnl.megapesni.com/get/online/JV3Hr_HWJMelpkbg97gcVg/1622426588/7be549df/2017/10/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9D%D0%B5%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Бери Меня",
+    songLink: "https://dnl.megapesni.com/get/online/Vcs5RRiJykOtHiCYV3Fq-A/1622426588/7be75889/2017/10/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%91%D0%B5%D1%80%D0%B8%20%D0%9C%D0%B5%D0%BD%D1%8F.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "АтоМы",
+    songLink: "https://dnl.megapesni.com/get/online/E9ozt_kpc6vwCQHQfyAjfw/1622426588/28268a22/2018/10/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%90%D1%82%D0%BE%D0%9C%D1%8B.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Not Enough For Me",
+    songLink: "https://dnl.megapesni.com/get/online/ztKtHXPdAylSyHW6w50iMA/1622426588/7bc2fabe/2017/08/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20Not%20Enough%20For%20Me.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Сама По Себе",
+    songLink: "https://dnl.megapesni.com/get/online/BVLp_T08s7vBwIw-IWt42Q/1622426795/7be7558e/2017/10/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%A1%D0%B0%D0%BC%D0%B0%20%D0%9F%D0%BE%20%D0%A1%D0%B5%D0%B1%D0%B5.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Принимай Меня",
+    songLink: "https://dnl.megapesni.com/get/online/zhCTaXbN8tGW-YSM4UZXLQ/1622426795/2a0fe850/2018/08/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9F%D1%80%D0%B8%D0%BD%D0%B8%D0%BC%D0%B0%D0%B9%20%D0%9C%D0%B5%D0%BD%D1%8F.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "SWIPE",
+    songLink: "https://dnl.megapesni.com/get/online/SMmxo8aFJbWZKPu7eYbY3g/1622426795/a3ddea06/2020/03/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20SWIPE.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Не Виновата",
+    songLink: "https://dnl.megapesni.com/get/online/TA4pPhn5k4phBSqG_JnQ2w/1622426795/417273d9/2019/12/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9D%D0%B5%20%D0%92%D0%B8%D0%BD%D0%BE%D0%B2%D0%B0%D1%82%D0%B0.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Орбит Без Сахара",
+    songLink: "https://dnl.megapesni.com/get/online/eCsPRQIez-PoYmgt7HWMdQ/1622426795/724d1218/2020/06/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9E%D1%80%D0%B1%D0%B8%D1%82%20%D0%91%D0%B5%D0%B7%20%D0%A1%D0%B0%D1%85%D0%B0%D1%80%D0%B0.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Губы",
+    songLink: "https://dnl.megapesni.com/get/online/FNpvBm-JQWU_LZ4HSzFRrA/1622426795/3407606d/2018/10/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%93%D1%83%D0%B1%D1%8B.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Проблема",
+    songLink: "https://dnl.megapesni.com/get/online/bePz5lcqmxn4exm93HxtKQ/1622426795/65d52630/2020/09/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9F%D1%80%D0%BE%D0%B1%D0%BB%D0%B5%D0%BC%D0%B0.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Она Не Боится",
+    songLink: "https://dnl.megapesni.com/get/online/L-J0bGprFfaGtML7okWZ0A/1622426966/7c532443/2018/03/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9E%D0%BD%D0%B0%20%D0%9D%D0%B5%20%D0%91%D0%BE%D0%B8%D1%82%D1%81%D1%8F.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Хейт",
+    songLink: "https://dnl.megapesni.com/get/online/JRU4rbZGrCJ_TkptTCB5hw/1622426966/17297014/2020/11/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%A5%D0%B5%D0%B9%D1%82.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Мои Люди Всегда Со Мной",
+    songLink: "https://dnl.megapesni.com/get/online/QQ27Ck3jWUmctaEhhAFoWg/1622426966/7c0f14c7/2017/11/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9C%D0%BE%D0%B8%20%D0%9B%D1%8E%D0%B4%D0%B8%20%D0%92%D1%81%D0%B5%D0%B3%D0%B4%D0%B0%20%D0%A1%D0%BE%20%D0%9C%D0%BD%D0%BE%D0%B9.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "X.O",
+    songLink: "https://dnl.megapesni.com/get/online/B9Ai8BpsvjCXyOg021gPuQ/1622426966/b53204c4/2020/08/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20X.O.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Чемпион",
+    songLink: "https://dnl.megapesni.com/get/online/yPMBCazU8bFAz4nEkjbq9g/1622426966/69800f9a/2018/05/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%A7%D0%B5%D0%BC%D0%BF%D0%B8%D0%BE%D0%BD.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Закрой Глаза",
+    songLink: "https://dnl.megapesni.com/get/online/oSAvSceJt3YXfOmU8QixeA/1622426966/7bca5b67/2017/09/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%97%D0%B0%D0%BA%D1%80%D0%BE%D0%B9%20%D0%93%D0%BB%D0%B0%D0%B7%D0%B0.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Эгоистка",
+    songLink: "https://dnl.megapesni.com/get/online/utlKZi9NPdx-F0J0vNaw9A/1622426966/dbd91db3/2018/10/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%AD%D0%B3%D0%BE%D0%B8%D1%81%D1%82%D0%BA%D0%B0.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Миром Правит Любовь",
+    songLink: "https://dnl.megapesni.com/get/online/RGD6z-dA_uKD6pFRaa1o1g/1622426966/c7de09c3/2018/11/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9C%D0%B8%D1%80%D0%BE%D0%BC%20%D0%9F%D1%80%D0%B0%D0%B2%D0%B8%D1%82%20%D0%9B%D1%8E%D0%B1%D0%BE%D0%B2%D1%8C.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Очень Хорошо",
+    songLink: "https://dnl.megapesni.com/get/online/k4B9k9lUmYlhb_ii0B2cWg/1622426966/e6bb769f/2019/01/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9E%D1%87%D0%B5%D0%BD%D1%8C%20%D0%A5%D0%BE%D1%80%D0%BE%D1%88%D0%BE.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Алкоголь",
+    songLink: "https://dnl.megapesni.com/get/online/HPBhiAiL2m8tOol8Vwkd2g/1622426966/9ffe9db0/2018/10/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%90%D0%BB%D0%BA%D0%BE%D0%B3%D0%BE%D0%BB%D1%8C.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Пятница",
+    songLink: "https://dnl.megapesni.com/get/online/brp2AwLHLHbTdu9a4mxQzg/1622426966/f0f3cf6f/2018/10/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9F%D1%8F%D1%82%D0%BD%D0%B8%D1%86%D0%B0.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Слабость",
+    songLink: "https://dnl.megapesni.com/get/online/R8sPRLEHdmG2X94jh4pnmg/1622426966/77525e2c/2018/10/Ольга%20Бузова%20-%20Слабость.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Женское Слово",
+    songLink: "https://dnl.megapesni.com/get/online/JXDcvLugDFlBkyzUHnREhQ/1622426966/9366707f/2019/03/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%96%D0%B5%D0%BD%D1%81%D0%BA%D0%BE%D0%B5%20%D0%A1%D0%BB%D0%BE%D0%B2%D0%BE.mp3",
+    photoLink: "",
+    "check": false,
+  },
+    
+  {
+    name: "Ненормальный Вайб",
+    songLink: "https://dnl.megapesni.com/get/online/Q6vBtZaByGZyYX-stbqk0Q/1622427287/7aac5b80/2020/10/%D0%9E%D0%BB%D1%8C%D0%B3%D0%B0%20%D0%91%D1%83%D0%B7%D0%BE%D0%B2%D0%B0%20-%20%D0%9D%D0%B5%D0%BD%D0%BE%D1%80%D0%BC%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9%20%D0%92%D0%B0%D0%B9%D0%B1.mp3",
+    photoLink: "",
+    "check": false,
   }
 ]
 
